f4c0624cfdf403a65dc82c258b66eee65725c29b
shrimon347 <shrimon1999@gmail.com>
2025-02-13 22:04:21 +0600

    feat(database): create user_registration database

---
+++ Task-1/db/init.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE IF NOT EXISTS user_registration;
+
+USE user_registration;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE
+);
