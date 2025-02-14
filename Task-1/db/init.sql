8f3c30d5fd9d8744058905b8183002fd3d1f39a5
shrimon347 <shrimon1999@gmail.com>
2025-02-14 18:30:57 +0600

    feat(database): create car table

--- Task-1/db/init.sql
+++ Task-1/db/init.sql
@@ -2,8 +2,19 @@ CREATE DATABASE IF NOT EXISTS user_registration;
 
 USE user_registration;
 
+--User table (for Task-!)
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE
+);
+
+--New Cars Table ( for Task-2)
+
+CREATE TABLE IF NOT EXISTS cars (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    brand VARCHAR(255) NOT NULL,
+    model VARCHAR(255) NOT NULL,
+    year INT NOT NULL,
+    price DECIMAL(10, 2) NOT NULL
 );
